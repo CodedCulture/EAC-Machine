a6cf621d3dc4b4fc9268cfda8c41db711c9456fa
Nick Krabbenhoeft <n@nkrabben.com>
2015-04-21 11:35:44 -0400

    small changes, dropped the ball here

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 from lxml import etree as ET
 import xlrd
 from datetime import date, datetime
+import dateutil.parser as parsedate
 import re
 
 
@@ -9,38 +10,46 @@ namespaces = {'ns':'isbn:1-931666-33-4', 'xlink': 'http://www.w3.org/1999/xlink'
 eacNS = '{urn:isbn:1-931666-33-4}'
 parser = ET.XMLParser(remove_blank_text=True)
 
+def parse(self, timestr, default=None,
+          ignoretz=False, tzinfos=None,
+          **kwargs):
+    return self._parse(timestr, **kwargs)
 
-def extractRecords(wkbkPath, sheetName):
-   xlData = xlrd.open_workbook(wkbkPath)
-   xlSkinnies = xlData.sheet_by_name(sheetName)
+parsedate.parser.parse = parse
 
-   totalRows = xlSkinnies.nrows - 1
 
-   for i in range(1, totalRows):
-      skinnyRow = xlSkinnies.row(i)
-      if skinnyRow[0].value == 0:
-         skinnyXML = createSkinnyXML(skinnyRow)
-         saveSkinnyXML = '.../out/' + skinnyRow[1].value + '.xml'
-         skinnyXML.write(saveSkinnyXML, encoding = 'utf-8', method = 'xml', xml_declaration=True)
+def extractRecords(wkbkPath, sheetName, type):
+	xlData = xlrd.open_workbook(wkbkPath)
+	xlSkinnies = xlData.sheet_by_name(sheetName)
 
+	totalRows = xlSkinnies.nrows - 1
+	recordsWritten = 0
+
+	for i in range(1, 10):
+		skinnyRow = xlSkinnies.row(i)
+		#print skinnyRow
+		if skinnyRow[0].value == 0:
+			createSkinnyXML(skinnyRow, type)
+			recordsWritten = recordsWritten + 1
+
+	print recordsWritten
 
 def createSkinnyXML(values, type):
 	#create control section with custom information
-	base = createBaseXML(values[1])
+	base = createBaseXML(values[1].value)
+	print values[1].value
+	print values
+	print base
 	root = base.getroot()
 
 	if type == 'corporate':
-		description = createCDescription(values, type)
-		root.append(description.getroot())
+		root.append(createCDescription(values, type).getroot())
 	elif type == 'person':
-		description = createPDescription(values, type)
-		root.append(description.getroot())
+		root.append(createPDescription(values, type).getroot())
 	else:
-		print 'Require "corporate" or "person" as second argument"
+		print 'Require "corporate" or "person" as second argument'
 	
-	
-
-	base.write('new/' + values[1] + '.xml', encoding = 'utf-8', method = 'xml', xml_declaration=True, pretty_print = True)
+	base.write('new/' + values[1].value + '.xml', encoding = 'utf-8', method = 'xml', xml_declaration=True, pretty_print = True)
 
 
 def createBaseXML(recordID):
@@ -57,7 +66,7 @@ def createBaseXML(recordID):
 	return(base)
 
 
-def newMaintenanceEvent(agent, event):
+def createMaintenanceEvent(agent, event):
 	#create base element
 	maintenance = ET.Element('maintenanceEvent')
 
@@ -76,8 +85,10 @@ def newMaintenanceEvent(agent, event):
    	return(maintenance)
 
 
-def createPDescription(values, type):
+def createCDescription(values, type):
 	#TODO
+	return('')
+
 
 def createPDescription(values, type):
 	cpfDescription = ET.parse('skinnyDescription.xml', parser)
@@ -85,48 +96,42 @@ def createPDescription(values, type):
 	#identity
 	cpfDescription.find('./identity/entityType').text = type
 	name = cpfDescription.find('./identity/nameEntry')
-	if values[2] != '':
-		name.append(createElement('part', values[2], 'localType', '100a'))
-	if values[3] != '':
-		name.append(createElement('part', values[3], 'localType', '100q'))
-	if values[4] != '':
-		name.append(createElement('part', values[4], 'localType', '100d'))
-	if values[5] != '':
+	if values[2].value != '':
+		name.append(createElement('part', values[2].value, 'localType', '100a'))
+	if values[3].value == 'lcnaf':
 		#uri?
-		name.append(createElement('authorizedForm', values[5]))
+		name.append(createElement('authorizedForm', 'lcnaf'))
 
 	#description
 	description = cpfDescription.find('./description')
 
 	#dates
 	existDates = description.find('.//dateRange')
-	if values[8] != '':
-		existDates.append(createElement('fromDate', values[8]))
+	if values[6].value:
+		existDates.append(createDateElement('fromDate', values[6].value))
 #### rewrite date converter
-	if values[9] != '':
-		existDates.append(createElement('toDate', values[9]))
+	if values[7].value:
+		existDates.append(createDateElement('toDate', values[7].value))
 
 	#bioghist
 	biogHist = description.find('.//biogHist')
-	if values[7] != '':
-		biogHist.append(createElement('abstract', values[7]))
-	if values[10] == 'TRUE':
+	relations = cpfDescription.find('./relations')
+	if values[5].value != '':
+		biogHist.append(createElement('abstract', values[5].value))
+	if values[8].value == True:
 		biogHist.append(createElement('p', 'This person was employed by the American Museum of Natural History'))
+		employee = createAMNHRelation(values[11].value)
+		if values[13].value != '':
+			employee.append(convertDateRange(values[13].value))
+		relations.append(employee)
 		
-###create cpfRelation for employee, trustee
-		#employee = ET.Element('cpfRelation')
-		#employee.set('cpfRelationType', 'identity')
-		#employee.set('xlink:arcrole', 'employeeOf')
-		#employee.set('xlink:href', 'amnh_1')
-		#employee.text = 
-		
-	if values[11] == 'TRUE':
+	if values[9].value == True:
 		biogHist.append(createElement('p', 'This person served as a trustee for the American Museum of Natural History'))
-	if values[12] != '':
-		biogHist.append(createElement('p', values[12]))
+	if values[10].value != '':
+		biogHist.append(createElement('p', values[10].value))
 
 	#relations
-	relations = cpfDescription.find('./relations')
+	
 
 
 	return(cpfDescription)
@@ -134,39 +139,73 @@ def createPDescription(values, type):
 
 def createElement(tag, value = '', attrib = '', attribValue = False):
 	element = ET.Element(tag)
-	element.text = value
+	element.text = unicode(value)
 	if attribValue:
 		element.set(attrib, attribValue)
 	return(element)
 
 
+def createDateElement(tag, value, approxValue = ''):
+	element = ET.Element(tag)
+	if approxValue != '':
+		value = approxValue
+	element.text = value
+	element.set('standardDate', convertDateString(value))
+	return(element)
+
+
 def convertDateString(date):
-	data = strip(date)
-	dateRange = re.search(date, r'(.+)-(.+)')
-	if dateRange:
-		#createElement('fromDate',dateRange.group(1), 'standardDate', convertDateString(dateRange.group(1)))
-		#createElement('toDate', dateRange.group(2), 'standardDate', convertDateString(dateRange.group(2)))
-		return('')
-	day = re.search(date, r',')
-	if day:
-		print datetime.strptime(date, '%B %d, %Y')
-		return('')
-	month = re.search(date, r'[A-Za-z]+\s\d\d\d\d')
-	if month:
-		print datetime.strptime(date, '%B %Y')
-		return('')
-	year = re.search(date, r'\d\d\d\d')
-	if year:
-		print datetime.strptime(date, '%Y')
-		return('')
+	ddd = parsedate.parser().parse(date, None, fuzzy = True)
+	print ddd
+	if ddd.day:
+		dstr = str(ddd.year)+'-'+str("%02d" % ddd.month)+'-'+str("%02d" % ddd.day)
+	elif ddd.month:
+		dstr =  str(ddd.year)+'-'+str("%02d" % ddd.month)
+	elif ddd.year:
+		dstr = str(ddd.year)
+	else:
+		print 'Unreadable date: ' + date
+	return(dstr)
+
+
+def convertDateRange(dateRange):
+	rangeMatch = re.search(r'(.*)-(.*)', dateRange)
+	if rangeMatch:
+		dates = ET.Element('dateRange')
+		fromDate = re.search(r'(.*)(\d\d\d\d)', rangeMatch.group(1))
+		toDate = re.search(r'(.*)(\d\d\d\d)', rangeMatch.group(2))
+		print fromDate, toDate
+		if fromDate:
+			dates.append(createDateElement('fromDate', fromDate.group(2), rangeMatch.group(1)))
+		if toDate:
+			dates.append(createDateElement('toDate', toDate.group(2), rangeMatch.group(2)))
+		return(dates)
+
+	else:
+		openMatch = re.search(r'(.*)(\d\d\d\d)', dateRange)
+		if openMatch:
+			dates = ET.Element('dateRange')
+			fromDate = re.search(r'\d\d\d\d', openMatch.group(2))
+			if fromDate:
+				dates.append(createDateElement('fromDate', fromDate.group(0), dateRange))
+			return(dates)
 
 
 
 
+def createAMNHRelation(dept = ''):
+	relationDesc = 'Employed by AMNH'
+	if dept != '':
+		relationDesc = 'Employed by AMNH in ' + dept
+	relation = ET.Element('cpfRelation')
+	relation.set('cpfRelationType', 'identity')
+
+	#need xlink namespace
+	relation.set('arcrole', 'employedBy')
+	relation.set('href', 'http://vocab.org/relationship/employedBy')
+	ET.SubElement(relation, 'descriptiveNote').text = relationDesc
+
+	return(relation)
 
 
-
-
-
-
-
+#URI source
