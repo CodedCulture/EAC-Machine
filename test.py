fcedef53952745ae106f7b746e01893dcb998439
Nick Krabbenhoeft <n@nkrabben.com>
2015-04-21 11:38:11 -0400

    new testing bits

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from functions import createSkinnyXML
+from functions import extractRecords
 
 testlist = ['status', 'recordId', 'name', 'full name', 'death', 'authorized', 'uri', 'title', 'dob', 'dod', 'staff', 'trustee', 'other', 'dep', 'depid', 'years', 'expedition']
 
-createSkinnyXML(testlist, 'person')
+#createSkinnyXML(testlist, 'person')
+extractRecords('../../datadev/data/MASTER_amnhPersons_macros.xlsm', 'amnhp_MASTER', 'person')
