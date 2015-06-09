f51b3db53f6e023c7f71ffc0bea7f7ea57d814db
Nick Krabbenhoeft <n@nkrabben.com>
2015-06-09 11:29:18 -0400

    fix import statement

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from lib.createEAC import extractRecords
+from src.createEAC import extractRecords
 
 #extractRecords('../../datadev/data/MASTER_amnhPersons_macros.xlsm', 'amnhp_MASTER', 'person')
 extractRecords('../../datadev/data/amnh_expeditions_20150315.xlsx', 'Sheet1', 'corporate')
